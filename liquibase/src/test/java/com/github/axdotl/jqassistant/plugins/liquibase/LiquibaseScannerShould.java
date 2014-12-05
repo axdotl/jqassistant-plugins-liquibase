5f5e8118373ae5018c6569b36facc99a0a93b572
Axel Köhler <axlkoe@gmx.net>
2014-12-05 15:16:50 +0100

    Test: Reuse Include node when scanning ChangeLog

--- liquibase/src/test/java/com/github/axdotl/jqassistant/plugins/liquibase/LiquibaseScannerShould.java
+++ liquibase/src/test/java/com/github/axdotl/jqassistant/plugins/liquibase/LiquibaseScannerShould.java
@@ -3,6 +3,7 @@ package com.github.axdotl.jqassistant.plugins.liquibase;
 import java.io.File;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,8 +24,10 @@ import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.LiquibaseDescr
 public class LiquibaseScannerShould extends AbstractPluginIT {
 
     private final static String DIR_NON_CHANGELOG = "nonChangeLog";
+    private final static String DIR_CHANGELOG = "changeLog";
     private final static String FILE_MASTER = "master.xml";
-    private final static String FILE_CHANGELOG_1 = "changeLog_1.xml";
+    private final static String FILE_VALID_CHANGELOG = "validChangeLog.xml";
+    private final static String FILE_INCLUDED_BY_MASTER = "includedByMaster.xml";
     private final static String SET_ATTR_ID_COMPLETE = "complete";
 
     private Scanner scanner;
@@ -66,13 +69,13 @@ public class LiquibaseScannerShould extends AbstractPluginIT {
     @Test
     public void detectIncludes() {
 
-        File masterFile = new File(testClassesDir, FILE_MASTER);
+        File masterFile = new File(testClassesDir, DIR_CHANGELOG + "/" + FILE_MASTER);
         store.beginTransaction();
 
-        ChangeLogDescriptor changeLogDescriptor = scanner.scan(masterFile, "/" + FILE_MASTER, null);
+        ChangeLogDescriptor changeLogDescriptor = scanner.scan(masterFile, "/" + DIR_CHANGELOG + "/" + FILE_MASTER, null);
         List<IncludeDescriptor> includes = changeLogDescriptor.getIncludes();
         Assert.assertNotNull("Includes expected.", includes);
-        Assert.assertEquals("Includes expected.", 3, includes.size());
+        Assert.assertEquals("Includes expected.", 2, includes.size());
 
         store.commitTransaction();
     }
@@ -83,10 +86,10 @@ public class LiquibaseScannerShould extends AbstractPluginIT {
     @Test
     public void applyChangeSetSubTags() {
 
-        File changeLogFile = new File(testClassesDir, FILE_CHANGELOG_1);
+        File changeLogFile = new File(testClassesDir, DIR_CHANGELOG + "/" + FILE_VALID_CHANGELOG);
         store.beginTransaction();
 
-        ChangeLogDescriptor changeLogDescriptor = scanner.scan(changeLogFile, "/" + FILE_CHANGELOG_1, null);
+        ChangeLogDescriptor changeLogDescriptor = scanner.scan(changeLogFile, "/" + DIR_CHANGELOG + "/" + FILE_VALID_CHANGELOG, null);
 
         List<ChangeSetDescriptor> changeSets = changeLogDescriptor.getChangeSets();
         Assert.assertNotNull("ChangeSets expected.", changeSets);
@@ -101,6 +104,36 @@ public class LiquibaseScannerShould extends AbstractPluginIT {
             }
         }
 
+        store.commitTransaction();
+    }
+
+    /**
+     * Test whether an existing Include-node is reused when scanning a ChangeLog i.s.o. creating a new/further node. Attributes has to be applied.
+     */
+    @Test
+    public void reuseIncludeNodeWhenScanningChangeLog() {
+
+        File changeLogDir = new File(testClassesDir, DIR_CHANGELOG);
+        File[] changeLogFiles = changeLogDir.listFiles();
+        store.beginTransaction();
+
+        // scan all files in folder
+        for (File file : changeLogFiles) {
+            scanner.scan(file, "/" + DIR_CHANGELOG + "/" + file.getName(), null);
+        }
+
+        // Verify
+        List<ChangeLogDescriptor> changeLogs = query("MATCH (inc:ChangeLog) RETURN inc").getColumn("inc");
+        for (ChangeLogDescriptor changeLog : changeLogs) {
+            String fileName = changeLog.getFileName();
+            if (StringUtils.endsWith(fileName, FILE_INCLUDED_BY_MASTER)) {
+                Assert.assertNotNull("ChangeLog is included in master.xml, so Include#file has to be set.", changeLog.getFile());
+                Assert.assertEquals("Include#file is an absolut path. So Include#file and File#fileName has to be equal.", changeLog.getFile(),
+                        fileName);
+            } else {
+                Assert.assertNull("ChangeLog is nowhere included, so Include#file has to be null.", changeLog.getFile());
+            }
+        }
         store.commitTransaction();
     }
 }
