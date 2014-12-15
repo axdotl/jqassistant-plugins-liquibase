b3b1f9a679938320c6d92ca8ab5a6080aa0274f7
Axel Köhler <axlkoe@gmx.net>
2014-12-15 21:55:52 +0100

    New test, verify that all knwon refactorings will be detected.

--- liquibase/src/test/java/com/github/axdotl/jqassistant/plugins/liquibase/LiquibaseScannerTest.java
+++ liquibase/src/test/java/com/github/axdotl/jqassistant/plugins/liquibase/LiquibaseScannerTest.java
@@ -19,6 +19,17 @@ import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.ChangeLogDescr
 import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.ChangeSetDescriptor;
 import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.IncludeDescriptor;
 import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.LiquibaseDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.AddColumnDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.AddForeignKeyDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.AddNotNullConstraintDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.AddPrimaryKeyDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.AddUniqueConstraintDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.CreateSequenceDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.CreateTableDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.DropColumnDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.DropConstraintDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.DropTableDescriptor;
+import com.github.axdotl.jqassistant.plugins.liquibase.descriptor.refactoring.SqlDescriptor;
 
 /**
  * Test class for the {@link LiquibaseScannerPlugin}.
@@ -32,6 +43,7 @@ public class LiquibaseScannerTest extends AbstractPluginIT {
     private final static String FILE_MASTER = "master.xml";
     private final static String FILE_VALID_CHANGELOG = "validChangeLog.xml";
     private final static String FILE_INCLUDED_BY_MASTER = "includedByMaster.xml";
+    private final static String FILE_ALL_KOWN_REFACTORINGS = "allKnownRefactorings.xml";
     private final static String SET_ATTR_ID_COMPLETE = "complete";
 
     private Scanner scanner;
@@ -152,6 +164,33 @@ public class LiquibaseScannerTest extends AbstractPluginIT {
                 Assert.assertNull("ChangeLog is nowhere included, so Include#file has to be null.", changeLog.getFile());
             }
         }
+
+        store.commitTransaction();
+    }
+
+    /**
+     * Tests whether known refactoring will be detected.
+     */
+    @Test
+    public void detectKnownRefactorings() {
+        File changeLogFile = new File(testClassesDir, DIR_CHANGELOG + "/" + FILE_ALL_KOWN_REFACTORINGS);
+        store.beginTransaction();
+
+        scanner.scan(changeLogFile, "/" + DIR_CHANGELOG + "/" + FILE_ALL_KOWN_REFACTORINGS, null);
+
+        // ////////////////////////////////////////////////////////////////////
+        // Verify
+        Mockito.verify(spyStore, Mockito.times(1)).create(AddColumnDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(AddForeignKeyDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(AddNotNullConstraintDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(AddPrimaryKeyDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(AddUniqueConstraintDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(CreateSequenceDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(CreateTableDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(DropColumnDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(DropConstraintDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(DropTableDescriptor.class);
+        Mockito.verify(spyStore, Mockito.times(1)).create(SqlDescriptor.class);
 
         store.commitTransaction();
     }
